4d7de1a2b32cabe226fde451bd010de89d14830b
codingwithsubhransu <subhransusekharrout987@gmail.com>
2025-08-06 20:08:36 +0530

    added third class

--- Learning/project_tutorial/main.py
+++ Learning/project_tutorial/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 def load_json():
@@ -20,3 +20,41 @@ def about():
 def view():
     data = load_json()
     return {"data": data}
+
+# @app.get('/patient/{patient_id}')
+# def get_patient(patient_id: str):
+#     data = load_json()
+#     if patient_id in data:
+#         return {"patient": data[patient_id]}
+#     else:
+#         return {'Error': "Patient not found."}
+
+'''By using Path Params'''
+# @app.get('/patient/{patient_id}')
+# def get_patient(patient_id: str = Path(..., description= "Enter Patient Id", example= "P001")):
+#     data = load_json()
+#     if patient_id in data:
+#         return {"patient": data[patient_id]}
+#     else:
+#         return {'Error': "Patient not found."}
+
+'''By Using HTTPException'''
+@app.get('/patient/{patient_id}')
+def get_patient(patient_id: str = Path(..., description="Enter Patient Id", example="P001")):
+    data = load_json()
+    if patient_id in data:
+        return {"patient": data[patient_id]}
+    raise HTTPException(status_code=404, detail="Patient not found.")
+
+@app.get('/sort')
+def sort_patients(sort_by: str = Query(..., description="Sort on the basis of height, weight, age or bmi"), order: str = Query("asc", description= "sort n asc and desc order")):
+    valid_sort_fields = ['height', 'weight', 'age', 'bmi']
+    if sort_by not in valid_sort_fields:
+        raise HTTPException(status_code=400, detail=f"Invalid sort field. Choose from {valid_sort_fields}.")
+    
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail="Order must be 'asc' or 'desc'.")
+    
+    data = load_json()
+    sorted_data = sorted(data.values(), key= lambda x: x.get(sort_by, 0), reverse=(order == 'desc'))
+    return {"sorted_data": sorted_data}
